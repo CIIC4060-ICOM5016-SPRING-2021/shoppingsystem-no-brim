1e33439ed4d4fe4397339403b1efb6c1f07cef46
unknown <francisco.vera@upr.edu>
2022-05-01 13:25:17 -0400

    front end

--- Frontend/src/AllProducts.js
+++ Frontend/src/AllProducts.js
@@ -3,13 +3,13 @@ import {Button, Card, Container, Modal, Tab} from "semantic-ui-react";
 
 function AllProducts(props) {
     console.log(props)
-    props.info.forEach(value => console.log(value.pname));
+    props.info.forEach(value => console.log(value.name));
     return props.info.map(value => {return <Card>
         <Card.Content>
-            <Card.Header>{value.pname}</Card.Header>
-            <Card.Meta>{value.pprice}</Card.Meta>
+            <Card.Header>{value.name}</Card.Header>
+            <Card.Meta>{value.price}</Card.Meta>
             <Card.Description>
-                {value.pname}
+                {value.description}
             </Card.Description>
         </Card.Content>
         <Card.Content extra>
--- Frontend/src/Products.js
+++ Frontend/src/Products.js
@@ -1,9 +1,10 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {Button, Card, Container, Modal, Tab} from "semantic-ui-react";
 import AllProducts from "./AllProducts";
+import axios from "axios";
 
 function Products() {
-    const [data, setData] = useState("show");
+    const [data, setData] = useState('');
     let random_info = [{"pname": "p1", "pprice": 1.01, "pdescription": "description"},
         {"pname": "p2", "pprice": 1.01, "pdescription": "description"},
         {"pname": "p3", "pprice": 1.01, "pdescription": "description"},
@@ -11,9 +12,20 @@ function Products() {
         {"pname": "p5", "pprice": 1.01, "pdescription": "description"},
         {"pname": "p6", "pprice": 1.01, "pdescription": "description"}];
 
+    const getProducts = () => {
+        axios.get("https://db-class-22.herokuapp.com//NO-BRIM/Products/products")
+            .then((response)=>{
+                setData(response.data)
+            })
+    }
+
+    useEffect(getProducts,[])
+
+    if (!data) return null;
+
 
     return <Card.Group>
-        <AllProducts info={random_info}/>
+        <AllProducts info={data}/>
     </Card.Group>
 }
 
